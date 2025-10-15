8c66bfbc84f1615efbd6a50e2f7617c22075d92f
deep7311 <deepakchoudhary73111@gmail.com>
2025-10-15 17:26:18 +0530

    frontend domain added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://integretr-frontend.vercel.app/"],
     credentials: true
 }));
 app.use(express.json());
