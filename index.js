2e99b72edbed10da71580b81102a65d433f41031
deep7311 <deepakchoudhary73111@gmail.com>
2025-10-15 17:56:11 +0530

    updated

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const app = express();
 //     credentials: true
 // }));
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}
+));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
